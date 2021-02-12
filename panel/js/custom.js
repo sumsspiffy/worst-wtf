76dc41f19d283f6ac5dd69d7f092d26c086c2430
Dillan <sumsspiffy@gmail.com>
2021-02-11 17:40:00 -0800

    panel changes

--- panel/js/custom.js
+++ panel/js/custom.js
@@ -3,4 +3,14 @@ $(document).ready(function() {
     $(".container").css({"visibility": "visible"})
     $(".container").fadeTo("slow", 1)
   })
+
+  $("#login").on("click", function() {
+    $('.register-tab').removeClass('active')
+    $('.login-tab').addClass('active')
+  })
+
+  $("#register").on("click", function() {
+    $('.register-tab').addClass('active')
+    $('.login-tab').removeClass('active')
+  })
 })
